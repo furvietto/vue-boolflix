74a0d5a1eb983b355338f628583f409425763c0c
furvietto <fulvio-94@hotmail.it>
2022-01-19 11:33:45 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+.<template>
+  <div class="container-fluid">
+      <div class="row justify-content-end">
+          <div class="col-3">
+              <input type="text">
+              <button>INVIA</button>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style>
+
+</style>
