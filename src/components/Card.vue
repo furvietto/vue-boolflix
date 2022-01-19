41b6660d31a7d13dfe0a6739e5a1491921ab6420
furvietto <fulvio-94@hotmail.it>
2022-01-19 21:07:32 +0100

    add fixing

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+.<template>
+   <li class="card col">
+    <div>TITLE: {{title}}</div>
+    <div>
+    TITOLO ORIGINALE: {{original}}
+    </div>
+    <div>
+        LINGUA: {{language}}
+    </div>
+    <div>
+        VOTO FINALE: {{vote}}
+    </div>  
+</li>
+</template>
+
+<script>
+export default {
+    name:"Card",
+
+    props: ["title","original","language","vote"]
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,34 +1,32 @@
 .<template>
   <div class="container">      
-              <ul v-if="inputError" class="list-inline row row-cols-6 gap-3 justify-content-center">
-                  <li
-                  class="card col"
-                  v-for="(film,index) in inputHeader" 
-                  :key="index"
-                  >
-                  <div>TITLE: {{film.title}}</div>
-                  <div>
-                    TITOLO ORIGINALE: {{film.original_title}}
-                  </div>
-                    <div>
-                       LINGUA: {{film.original_language}}
-                    </div>
-                  <div>
-                     VOTO FILM: {{film.vote_average}}
-                  </div>
-                  
-                  </li>
+              <ul v-if="inputError == true" class="list-inline row row-cols-6 gap-3 justify-content-center">
+                  <Card 
+                    v-for="(film,index) in inputHeader" 
+                    :key="index"
+                    :title="film.title"
+                    :original="film.original_title"
+                    :language="film.original_language"
+                    :vote="film.vote_average"
+                  />
               </ul>
-              <div v-else>
-                  Ricerca non correlata
+              <div class="row justify-content-center" v-else-if="inputError == false">
+                  <div class="col-2">
+                       Ricerca non correlata...
+                  </div>                
               </div>
   </div>
 </template>
 
 <script>
+import Card from './Card.vue'
 
 export default {
     name:"Main",
+
+    components:{
+        Card,
+    },
 
     props: ["inputHeader","inputError"],
 
